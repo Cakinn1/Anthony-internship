4e6670f023833c656c8f516cc9ae2deafc842341
anthony <anthonycakins@gmail.com>
2023-07-30 17:42:38 +1000

    fixed explore bug

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -7,6 +7,9 @@ import "../../css/styles/style.css";
 import Slider from "react-slick";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
+
+// git hub test <===
+
 const HotCollections = () => {
   const [posts, nftPost] = useState([]);
   const [loading, setLoading] = useState(true);
